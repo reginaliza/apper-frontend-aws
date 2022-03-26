64dd658d3eb48e97e144acf697cca79d60c9045f
Regina Liza Diloy <reginaliza.diloy@gcash.com>
2022-03-26 10:53:49 +0800

    updated hw3

--- HW1/HW1-3.js
+++ HW1/HW1-3.js
@@ -1,8 +1,13 @@
 function reverse(string) {
+  if(typeof string == 'string') {
     var newString = "";
     for (var i = string.length - 1; i >= 0; i--) {
         newString += string[i];
     }
     return newString;
+  } else
+      return
+    
 }
 reverse('regina')
+reverse(1)
